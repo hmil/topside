cd9ac014cbc67b201fe759a5792918b017dcad00
Hadrien Milano <hadrien.milano@gmail.com>
2017-08-01 23:04:50 +0200

    feat(cli): add --outDir and --baseDir options
    --outDir sets the base output directory. The file structure in outDir is the same as the input files
    relative to --baseDir (or cwd if --baseDir is not set)

--- src/cli.ts
+++ src/cli.ts
@@ -4,8 +4,11 @@ import * as minimist from 'minimist';
 import * as fs from 'fs';
 import * as path from 'path';
 import * as glob from 'glob';
+import * as mkdirp from 'mkdirp';
 
 let argv = minimist(process.argv.slice(2));
+let outDir: string | null = null;
+let baseDir = process.cwd();
 
 function processFile(arg: string): void {
     const file = path.resolve(arg);
@@ -17,10 +20,27 @@ function processFile(arg: string): void {
     const filePath = path.parse(arg);
     filePath.ext = ".ts";
     filePath.base = filePath.name + filePath.ext;
+    if (outDir) {
+        filePath.dir = path.resolve(outDir, path.relative(baseDir, filePath.dir));
+    }
+    mkdirp(filePath.dir, function (err: any) {
+        if (err) {
+            console.error(err);
+        } else {
+            const dest = path.resolve(path.format(filePath));
+            fs.writeFileSync(dest, compiled.code + "\n//# sourceMappingURL=" + filePath.name + '.ts.map');
+            fs.writeFileSync(dest + '.map', compiled.map);
+        }
+    });
 
-    const dest = path.resolve(path.format(filePath));
-    fs.writeFileSync(dest, compiled.code + "\n//# sourceMappingURL=" + filePath.name + '.ts.map');
-    fs.writeFileSync(dest + '.map', compiled.map);
+}
+
+if (argv.outDir) {
+    outDir = path.resolve(process.cwd(), argv.outDir);
+}
+
+if (argv.baseDir) {
+    baseDir = path.resolve(process.cwd(), argv.baseDir);
 }
 
 for (let i = 0 ; i < argv._.length ; i++) {
