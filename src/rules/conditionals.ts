65e16976f72526fa545cac67349bf20e6efe30eb
Hadrien Milano <hadrien.milano@gmail.com>
2018-10-27 00:18:48 +0200

    fix(rules): remove unreachable code in conditionals (#19)

--- src/rules/conditionals.ts
+++ src/rules/conditionals.ts
@@ -2,12 +2,30 @@ import { Context, Fragment, Rule } from '../CompilerInterface';
 import { ElseFragment, ElseIfFragment, EndIfFragment, IfFragment } from '../fragments/conditionals';
 import { Token } from '../Token';
 
+declare module "../CompilerInterface" {
+    interface Context {
+        conditionals: {
+            statementStack: Array<{
+                hasElse: boolean
+            }>
+        }
+    }
+}
+
 
 export const IfRule: Rule = {
     name: "if",
 
+    initContext(ctx: Context): void {
+        ctx.conditionals = {
+            statementStack: []
+        };
+    },
+
     analyze(ctx: Context, t: Token): Fragment {
-        ctx;
+        ctx.conditionals.statementStack.splice(0, 0, {
+            hasElse: false
+        });
         return new IfFragment(t, t.data);
     }
 };
@@ -16,7 +34,7 @@ export const ElseRule: Rule = {
     name: "else",
 
     analyze(ctx: Context, t: Token): Fragment {
-        ctx;
+        ctx.conditionals.statementStack[0].hasElse = true;
         return new ElseFragment(t);
     }
 };
@@ -24,8 +42,7 @@ export const ElseRule: Rule = {
 export const ElseIfRule: Rule = {
     name: "elseif",
 
-    analyze(ctx: Context, t: Token): Fragment {
-        ctx;
+    analyze(_: Context, t: Token): Fragment {
         return new ElseIfFragment(t, t.data);
     }
 };
@@ -34,7 +51,9 @@ export const EndIfRule: Rule = {
     name: "endif",
 
     analyze(ctx: Context, t: Token): Fragment {
-        ctx;
-        return new EndIfFragment(t);
+        // Skips the return statement if there is an `else` block to avoid generating unreachable code.
+        const returnStatement = !ctx.conditionals.statementStack[0].hasElse;
+        ctx.conditionals.statementStack.splice(0, 1);
+        return new EndIfFragment(t, returnStatement);
     }
 };
